978b8063a2e4f5b992f20b003004ba4c4b2d8df0
Adon <adon.rosen@gmail.com>
2020-10-21 11:11:52 -0500

    Adding midterm

---
+++ Code/Midterm.R
@@ -0,0 +1,12 @@
+# ---- load-packages --------------------------------------------------------
+library("tidyverse")
+library("knitr")
+library("kableExtra")
+library("reshape2")
+library("lavaan")
+library("semPlot")
+
+# ---- helpful-links --------------------------------------------------------
+# http://biostat.mc.vanderbilt.edu/wiki/pub/Main/ContinuingEdu/CTSaunders_CausalDiagrams.pdf
+
+
